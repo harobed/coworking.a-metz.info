06617a7a00edd564e11f90a13f5423742a4ef027
Stéphane Klein <contact@stephane-klein.info>
2015-01-08 12:21:57 +0100

    Ajout de la bannière d’information

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -5,4 +5,11 @@ $(document).ready(function() {
         'content': '&nbsp;<i class="fa fa-link"></i>',
         'position': 'append'
     });
+
+    $.get('http://membres.coworking.a-metz.info/website_banner/public', function(data) {
+        if (data != '') {
+            $('#website_banner').show();
+            $('#website_banner_inner').html(data);
+        }
+    })
 });
